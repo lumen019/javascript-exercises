7e83e9b101601d4285140b4bf187fb9e1c74282b
lumen019 <237933659+lumen019@users.noreply.github.com>
2025-12-18 00:47:01 -0700

    modify files for js loops and arrays exercises 07-11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeF) {
+    let result = ((degreeF - 32) * 5/9);
+    return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeC) {
+    let result = ((degreeC * (9/5)) + 32);
+    return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
