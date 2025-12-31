e3eecdf0a2f720259b401fa16b08d8efc85bd1d4
Lumen019 <237933659+lumen019@users.noreply.github.com>
2025-12-31 13:56:44 -0700

    modify js for lesson 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0; 
+  arr.forEach(element => {
+    total += element;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product =1;
+  for (let i=0; i < arr.length; i++){
+    product = product * arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  for (let i=num; i > 0; i--){
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
