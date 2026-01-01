0ed6242fa64c433d8a97a7bbf80dd52cab21f083
Lumen019 <237933659+lumen019@users.noreply.github.com>
2025-12-31 18:25:41 -0700

    modifiy JS for lesson 13

--- 13_factorial/factorial.js
+++ 13_factorial/factorial.js
@@ -1,5 +1,11 @@
-const factorial = function() {
-
+const factorial = function(n) {
+    if(n === 0 || n === 1){
+        return 1;
+    } else if (Number.isInteger(n) && n > 0){
+        return n * factorial(n - 1)
+    } else {
+        return undefined;
+    }
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const arr = word //makes word lowercase then splits each letter into an array.
+        .toLowerCase()
+        .split('')
+    const lettersOnlyArr = arr.filter(element => {  //filters only a-z and 0-9
+        return element.match(/[a-z0-9]/);
+    });
+    const lettersOnly = lettersOnlyArr.join(""); //turns lettersOnlyArr into a string
+    const reverseLetters = lettersOnlyArr  //reverses letterOnlyArr then turns reverse into a string
+        .reverse()
+        .join("");
+    if (lettersOnly === reverseLetters) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
